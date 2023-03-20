e1f3746420590c837679d2e6737f616ba363d228
Carlos Randino <crandino.naduku@gmail.com>
2023-03-20 16:19:58 +0100

    Adding some preliminary Vector extensions

---
+++ Assets/Tools/Scripts/VectorSwizzleExtensions.cs
@@ -0,0 +1,11 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+namespace Greenyas.VectorExtensions.Swizzle
+{
+    public static class VectorSwizzleExtensions
+    {
+        public static Vector3 SwizzleX_Y(this Vector2 v, float yValue = 0) => new Vector3(v.x, yValue, v.y);
+        public static Vector2 SwizzleXZ(this Vector3 v, float yValue = 0) => new Vector2(v.x, v.z);
+    }
+}
